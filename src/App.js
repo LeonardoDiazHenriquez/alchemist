64d716fd940dbf22c5210d3cd68123f88d1c2d06
Leonardo Diaz Henriquez <leonardo.diazhenriquez@gmail.com>
2020-12-31 15:11:51 -0300

    Added basic layout

--- src/App.js
+++ src/App.js
@@ -2,23 +2,41 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const alphabet = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L','M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T','U','V','W','X','Y','Z'];
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <div id="workspace">
+        <div id="left"></div>
+        <div id="center">
+            <ul>
+              {
+                alphabet.map(letter => (<li>{letter}</li>)) 
+              }
+            </ul>
+        </div>
+        <div id="right">
+          <div className="element">
+            <div className="img"><img src="assets/air.jpg" /></div>
+            <div className="text">air</div>
+          </div>
+          <div className="element">
+          <div className="img"><img src="assets/earth.jpg" /></div>
+            <div className="text">earth</div>
+          </div>
+          <div className="element">
+          <div className="img"><img src="assets/fire.jpg" /></div>
+            <div className="text">fire</div>
+          </div>
+          <div className="element">
+          <div className="img"><img src="assets/water.jpg" /></div>
+            <div className="text">water</div>
+          </div>
+        
+        </div>
+      </div>
     </div>
+
   );
 }
 
